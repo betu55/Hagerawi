470681683c1f135de7a434fc82b7fef1b2c6b682
luhana daniel <se.luhana.daniel@gmail.com>
2021-09-01 17:08:54 +0300

    admin feed posting page basics

---
+++ lib/pages/feeds_post.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:friendly_chat/files/cards.dart';
+import 'package:friendly_chat/files/sidebar.dart';
+
+void main() {
+  runApp(
+    const FriendlyChatApp(),
+  );
+}
+
+class FriendlyChatApp extends StatelessWidget {
+  const FriendlyChatApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FriendlyChat',
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            backgroundColor: darkgreyHex,
+            drawer: Navbar(),
+            appBar: AppBar(
+              title: Text(
+                "Add posts",
+                style: TextStyle(color: warmOrangeHex),
+              ),
+              centerTitle: true,
+              backgroundColor: darkBlueHex,
+              backwardsCompatibility: true,
+              iconTheme: IconThemeData(color: warmOrangeHex),
+            ),
+            // floatingActionButton: FloatingActionButton(
+            //   onPressed: () {
+            //     setState(() {
+            //       num += 1;
+            //     });
+            //   },
+            //   focusElevation: 5,
+            //   backgroundColor: darkBlueHex,
+            //   child: Icon(Icons.add),
+            // ),
+            body: const MyStatelessWidget()));
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+        color: darkBlueHex,
+        shadowColor: Colors.black,
+        elevation: 5,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const ListTile(
+              leading: Icon(Icons.album),
+              title: Text(
+                "The Enchanted Nightingale",
+              ),
+              subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+            ),
+            
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
