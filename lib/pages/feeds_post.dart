f8e2eba36c5dbbda543f10c7d37c856d17b8b055
luhana daniel <se.luhana.daniel@gmail.com>
2021-09-02 15:57:57 +0300

    changed to table for layouts

--- lib/pages/feeds_post.dart
+++ lib/pages/feeds_post.dart
@@ -1,109 +1,147 @@
-import 'package:flutter/material.dart';
-import 'package:friendly_chat/files/cards.dart';
-import 'package:friendly_chat/files/sidebar.dart';
+import 'package:flutter/material.dart';  
+import 'package:chat_one/components/sidebar.dart';
+import 'package:chat_one/components/cards.dart'; 
+void main() {runApp(MyApp());}  
+  
+class MyApp extends StatefulWidget {  
+  @override  
+  _DataTableExample createState() => _DataTableExample();  
+}  
+  
+class _DataTableExample extends State<MyApp> {  
+  @override  
+  Widget build(BuildContext context) {  
+    return MaterialApp(  
+      home: Scaffold(  
+          appBar: AppBar(  
+            backgroundColor: Colors.grey.shade700,
+            title: Center(child: Text('New Feeds ',
+            
+            style: TextStyle(
+              
+                  fontSize: 30
+            ),)),  
+          ),  
+          body: 
+        
 
-void main() {
-  runApp(
-    const FriendlyChatApp(),
-  );
-}
+          Container(
+            
+            
+            child: Center(
+              child: ListView(children: <Widget>[  
+                
+                DataTable(  
+                  columns: [  
+                    DataColumn(label: Text(  
+                        'Post Content',  
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)  
+                    )),  
+                    DataColumn(label: Text(  
+                        '',  
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)  
+                    )),  
+                     
+                  ],  
+                  rows: [  
+                    DataRow(cells: [  
+                      
+                      DataCell( Padding(
+                            padding: EdgeInsets.all(2.0),
+                            
+                            child: TextField(
+                          
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Title',
+                          ),
+                        )
+  ) )
+                       
+                    ]),  
+                    DataRow(cells: [  
+                      
+                      DataCell(Padding(
+    padding: EdgeInsets.all(2.0),
+    
+    child: TextField(
+  
+  decoration: InputDecoration(
+    border: OutlineInputBorder(),
+    labelText: 'Content',
+  ),
+)
 
-class FriendlyChatApp extends StatelessWidget {
-  const FriendlyChatApp({Key? key}) : super(key: key);
+  )),  
+                      
+                    ]),  
+                    DataRow(cells: [  
+                      
+                      DataCell(Padding(
+    padding: EdgeInsets.all(2.0),
+    
+    child: TextField(
+  
+  decoration: InputDecoration(
+    border: OutlineInputBorder(),
+    labelText: 'Author',
+  ),
+)
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FriendlyChat',
-      home: ChatScreen(),
-    );
-  }
-}
+  )),  
+                        
+                    ]),  
+                    DataRow(cells: [  
+                      
+                      DataCell(Padding(
+    padding: EdgeInsets.all(2.0),
+    
+    child: TextField(
+  
+  decoration: InputDecoration(
+    border: OutlineInputBorder(),
+    labelText: 'Title',
+  ),
+)
 
-class ChatScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            backgroundColor: darkgreyHex,
-            drawer: Navbar(),
-            appBar: AppBar(
-              title: Text(
-                "Add posts",
-                style: TextStyle(color: warmOrangeHex),
-              ),
-              centerTitle: true,
-              backgroundColor: darkBlueHex,
-              backwardsCompatibility: true,
-              iconTheme: IconThemeData(color: warmOrangeHex),
-            ),
-            // floatingActionButton: FloatingActionButton(
-            //   onPressed: () {
-            //     setState(() {
-            //       num += 1;
-            //     });
-            //   },
-            //   focusElevation: 5,
-            //   backgroundColor: darkBlueHex,
-            //   child: Icon(Icons.add),
-            // ),
-            body: const MyStatelessWidget()));
-  }
-}
-
-class MyStatelessWidget extends StatelessWidget {
-  const MyStatelessWidget({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-        margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
-        color: darkBlueHex,
-        shadowColor: Colors.black,
-        elevation: 5,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            const ListTile(
-              leading: Icon(Icons.album),
-              title: Text(
-                "The Enchanted Nightingale",
-              ),
-              subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                Expanded(
-                    child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter a search term',
-                  ),
-                )),
-                const SizedBox(width: 8),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                TextButton(
-                  child: const Text('BUY TICKETS'),
-                  onPressed: () {/* ... */},
-                ),
-                const SizedBox(width: 8),
-                TextButton(
-                  child: const Text('LISTEN'),
-                  onPressed: () {/* ... */},
-                ),
-                const SizedBox(width: 8),
-              ],
-            ),
-          ],
-        ),
+  )),  
+                        
+                    ]),  
+                  ],  
+                ),  
+               Container(child:Padding(
+    padding: EdgeInsets.fromLTRB(400, 50, 400, 0),
+    
+   child: ElevatedButton(
+    child: Text('Submit'),
+    style: TextButton.styleFrom(
+      textStyle: TextStyle(
+          color: Colors.black,
+          fontSize: 40,
+          fontStyle: FontStyle.italic
       ),
-    );
-  }
-}
+      primary: Colors.white,
+      backgroundColor: Colors.grey.shade700,
+      onSurface: Colors.grey,
+    ),
+    onPressed: () {
+      print('Pressed');
+    },
+  )
+
+  )
+            
+            )
+              ]
+              
+              ),
+              
+            ),
+            
+          
+          )
+          
+      ),  
+    );  
+  }  
+} 
