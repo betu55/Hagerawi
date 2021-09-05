442c746d7ef1a833cb155fbd1d46d8a4a30e3b00
ludaze <56473410+ludaze@users.noreply.github.com>
2021-09-06 01:19:51 +0300

    updated events with column layout

--- lib/pages/events_post.dart
+++ lib/pages/events_post.dart
@@ -1,109 +1,237 @@
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(EventPost());
 }
 
-class MyApp extends StatefulWidget {
+class EventPost extends StatelessWidget {
   @override
-  _DataTableExample createState() => _DataTableExample();
-}
-
-class _DataTableExample extends State<MyApp> {
-  @override
+  
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Events post',
       home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.grey.shade700,
-            title: Center(
-                child: Text(
-              'Post Events',
-              style: TextStyle(fontSize: 30),
-            )),
-          ),
-          body: Container(
-            child: Center(
-              child: ListView(children: <Widget>[
-                DataTable(
-                  columns: [
-                    DataColumn(
-                        label: Text('Post Content',
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold))),
-                    DataColumn(
-                        label: Text('',
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold))),
-                  ],
-                  rows: [
-                    DataRow(cells: [
-                      DataCell(Text('Event name')),
-                      DataCell(Padding(
-                          padding: EdgeInsets.all(2.0),
-                          child: TextField(
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Event name',
-                            ),
-                          )))
-                    ]),
-                    DataRow(cells: [
-                      DataCell(Text('Place')),
-                      DataCell(Padding(
-                          padding: EdgeInsets.all(2.0),
-                          child: TextField(
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Place',
-                            ),
-                          ))),
-                    ]),
-                    DataRow(cells: [
-                      DataCell(Text('Description')),
-                      DataCell(Padding(
-                          padding: EdgeInsets.all(2.0),
-                          child: TextField(
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Desc',
-                            ),
-                          ))),
-                    ]),
-                    DataRow(cells: [
-                      DataCell(Text('Date')),
-                      DataCell(Padding(
-                          padding: EdgeInsets.all(2.0),
-                          child: TextField(
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Date',
-                            ),
-                          ))),
-                    ]),
-                  ],
-                ),
-                Container(
-                    child: Padding(
-                        padding: EdgeInsets.fromLTRB(400, 50, 400, 0),
-                        child: ElevatedButton(
-                          child: Text('Submit'),
-                          style: TextButton.styleFrom(
-                            textStyle: TextStyle(
-                                color: Colors.black,
-                                fontSize: 40,
-                                fontStyle: FontStyle.italic),
-                            primary: Colors.white,
-                            backgroundColor: Colors.grey.shade700,
-                            onSurface: Colors.grey,
-                          ),
-                          onPressed: () {
-                            print('Pressed');
-                          },
-                        )))
-              ]),
-            ),
+        appBar:AppBar(
+          backgroundColor: Colors.greenAccent,
+          title: const Text('Post Events'),
+        ),
+        body: Center(
+            child: Container(
+              child: ListView(
+  padding: const EdgeInsets.all(40),
+  children: <Widget>[
+    
+    Container(
+      height: 100,
+      
+      child: Center(
+        
+        child: Row(
+        children:const <Widget>[
+        Expanded(
+          child: Padding(padding:
+          EdgeInsets.fromLTRB(0, 0, 30, 0),
+          child: const Align(
+            alignment:Alignment.centerRight,
+            child:Text('Event Name',
+          style: TextStyle(
+            fontSize: 25
+            )
+          )
           )),
-    );
+          ),
+        Expanded(
+          child: TextField(
+            cursorHeight: 15,
+            style: TextStyle(
+              fontSize: 15,
+              height: 2,
+              
+            ),
+          decoration: InputDecoration(
+            
+            
+            border: OutlineInputBorder(),
+            labelText: 'Event Name',
+          
+          ),
+          )
+        )
+        ],
+        
+
+        ),
+        )
+    ),
+    
+    Container(
+      height: 100,
+      
+      child: Center(
+        
+        child: Row(
+        children:const <Widget>[
+        Expanded(
+          child: Padding(padding:
+          EdgeInsets.fromLTRB(0, 0, 30, 0),
+          child: const Align(
+            alignment:Alignment.centerRight,
+            child:Text('Place',
+          style: TextStyle(
+            fontSize: 25
+            )
+          )
+          )),
+          ),
+        Expanded(
+          child: TextField(
+            cursorHeight: 15,
+            style: TextStyle(
+              fontSize: 15,
+              height: 2,
+              
+            ),
+          decoration: InputDecoration(
+            
+            
+            border: OutlineInputBorder(),
+            labelText: 'Place',
+          
+          ),
+          )
+        )
+        ],
+        
+
+        ),
+        )
+    ),
+    Container(
+      height: 100,
+      
+      child: Center(
+        
+        child: Row(
+        children:const <Widget>[
+        Expanded(
+          child: Padding(padding:
+          EdgeInsets.fromLTRB(0, 0, 30, 0),
+          child: const Align(
+            alignment:Alignment.centerRight,
+            child:Text('Description',
+          style: TextStyle(
+            fontSize: 25
+            )
+          )
+          )),
+          ),
+        Expanded(
+          child: TextField(
+            cursorHeight: 15,
+            style: TextStyle(
+              fontSize: 15,
+              height: 2,
+              
+            ),
+          decoration: InputDecoration(
+            
+            
+            border: OutlineInputBorder(),
+            labelText: 'Description',
+          
+          ),
+          )
+        )
+        ],
+        ),
+        )
+    ),
+   
+    
+     Container(
+      height: 100,
+      
+      child: Center(
+        child: Row(
+        children:const <Widget>[
+        Expanded(
+          child: Padding(padding:
+          EdgeInsets.fromLTRB(0, 0, 30, 0),
+          child: const Align(
+            alignment:Alignment.centerRight,
+            child:Text('Date',
+          style: TextStyle(
+            fontSize: 25
+            )
+          )
+          )),
+          ),
+        Expanded(
+          child: TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Date',
+          
+          ),
+          )
+        )
+        ],
+        
+
+        ),
+        )
+        
+    ),
+    
+    
+    Container(
+      height: 500,
+      alignment: Alignment.topCenter,
+      child:Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          ElevatedButton(
+            
+            style: ButtonStyle(
+              
+              alignment: Alignment.centerRight,
+            ),
+            onPressed:haya,
+            child: const Text('Submit'),
+          ),
+        ],
+      )
+      
+      
+    ),
+    
+     
+  ],
+)
+)
+  
+            ,)
+            ,
+        )
+
+  
+        );
+    
   }
 }
+void haya(){
+    print("text");
+  
+  }
+
+  /// Creates a [Category].
+  ///
+  /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
+  /// the UI, and the icon that represents it (e.g. a ruler).
+  // While the @required checks for whether a named parameter is passed in,
+  // it doesn't check whether the object passed in is null. We check that
+  // in the assert statement.
