362c3f4c1c6b3834bc4de37582633b3978ec3919
betu55 <se.bemenet.semegnew@gmail.com>
2021-09-09 23:31:19 +0300

    fixed conflict #2

--- lib/admin_events/admin_feeds_event_and_state.dart
+++ lib/admin_events/admin_feeds_event_and_state.dart
@@ -1,22 +1,24 @@
-// import 'dart:async';
+import 'dart:async';
 
-// import 'package:hagerawi_app/admin_feeds/admin_feeds_model.dart';
+import 'package:hagerawi_app/admin_feeds/admin_feeds_model.dart';
 
-// // event base class
-// abstract class AdminFeedEvent {}
+// event base class
+abstract class AdminFeedEvent {}
 
-// class PostAdminFeedsEvent extends AdminFeedEvent {
-//   final String title;
-//   final String desc;
-//   final String detail;
-//   final String author;
+class PostAdminFeedsEvent extends AdminFeedEvent {
+  final String title;
+  final String desc;
+  final String detail;
+  final String author;
 
-  
-//   PostAdminFeedsEvent({required this.title, required this.desc,
-//   required this.detail,required this.author});
-// }
+  PostAdminFeedsEvent(
+      {required this.title,
+      required this.desc,
+      required this.detail,
+      required this.author});
+}
 
-// class SearchFeedEvent extends AdminFeedEvent {}
+class SearchFeedEvent extends AdminFeedEvent {}
 
 
-// // state base class
+// state base class
