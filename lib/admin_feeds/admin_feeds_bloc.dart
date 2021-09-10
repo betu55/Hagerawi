0e7c5d31f9086bbcdce12bf11c72bca839a4793a
ludaze <56473410+ludaze@users.noreply.github.com>
2021-09-11 01:07:48 +0300

    bloc structure added removed states

--- lib/admin_feeds/admin_feeds_bloc.dart
+++ lib/admin_feeds/admin_feeds_bloc.dart
@@ -2,36 +2,40 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hagerawi_app/admin_feeds/admin_feeds_event_and_state.dart';
 import 'package:hagerawi_app/admin_feeds/admin_feeds_model.dart';
 import 'package:hagerawi_app/admin_feeds/admin_feeds_repo.dart';
+import 'package:hagerawi_app/admin_feeds/admin_feeds_state.dart';
+import 'package:hagerawi_app/pages/events_post.dart';
 
 class AdminFeedBloc extends Bloc<AdminFeedEvent, AdminFeedState> {
-  AdminFeedRepo feedRepo;
-  AdminFeedBloc(this.feedRepo) : super(AdminFeedsNotUploaded());
+  AdminFeedRepo adminFeedRepo;
+  AdminFeedBloc(this.adminFeedRepo) : super(FeedsUpload());
 
   @override
   Stream<AdminFeedState> mapEventToState(AdminFeedEvent event) async* {
-    if (event is FeedsUploaded) {
+    // on the case that our user opens the page for the first time
+    if (event is PostAdminFeedsEvent) {
+      final title= event.props[0].toString();
+      final author= event.props[1].toString();
+      final desc= event.props[2].toString();
+      final detail= event.props[3].toString();
       // this is the first state shown on the UI until the below try-catch code fetches data from our repository layer
-      final title = event.props[0].toString();
-      final author = event.props[1].toString();
-      final content = event.props[2].toString();
-      final detailed = event.props[3].toString();
-
       yield FeedsUploading();
+      await Future.delayed(Duration(seconds: 2));
 
-      await Future.delayed(Duration(seconds: 15));
-
+      // await Future.delayed(Duration(seconds: 15));
       try {
-        AdminFeedsModel feed =
-            await feedRepo.postFeeds(title, author, content, detailed);
-        print("at the bloc");
-        try {
-          yield FeedsUploaded(title, author, content, detailed);
-        } catch (e) {
+        //final feeds = await adminFeedRepo.addFeed(authorController.text.toString());
+        AdminFeedsModel feed = await adminFeedRepo.addFeed(title,author,desc,detail);
+        // print(feeds[0].detailed);
+        //yield FeedsUploaded(feeds);
+        try{
+          yield FeedsUploaded(feed.title, feed.author, feed.desc, feed.detailed);
+        }catch(e){
           print(e);
         }
-      } catch (e) {
-        yield UploadingFailed(errorMsg: e.toString());
+      } catch (err) {
+        yield AdminFeedsNotUploaded(errorMsg:err.toString());
       }
     }
+
   }
 }
